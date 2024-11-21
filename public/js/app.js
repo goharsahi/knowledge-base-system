5b2fb01cc26bee989fb9a6934b94d8c7531113f8
goharsahi <goharsarfraz@gmail.com>
2024-11-21 14:43:54 +0500

    configured tailwind and livewire

---
+++ public/js/app.js
@@ -0,0 +1,54 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/css/app.css":
+/*!*******************************!*\
+  !*** ./resources/css/app.css ***!
+  \*******************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/dist/cjs.js):\nD:\\laragon\\www\\KnowledgeBaseSystem\\postcss.config.js:1\nexport default {\n^^^^^^\n\nSyntaxError: Unexpected token 'export'\n    at Object.compileFunction (node:vm:360:18)\n    at wrapSafe (node:internal/modules/cjs/loader:1048:15)\n    at Module._compile (node:internal/modules/cjs/loader:1083:27)\n    at Module._extensions..js (node:internal/modules/cjs/loader:1173:10)\n    at Module.load (node:internal/modules/cjs/loader:997:32)\n    at Module._load (node:internal/modules/cjs/loader:838:12)\n    at Module.require (node:internal/modules/cjs/loader:1021:19)\n    at module.exports (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\import-fresh\\index.js:32:59)\n    at loadJs (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\loaders.js:16:18)\n    at Explorer.loadFileContent (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\Explorer.js:84:32)\n    at Explorer.createCosmiconfigResult (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\Explorer.js:89:36)\n    at Explorer.loadSearchPlace (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\Explorer.js:70:31)\n    at async Explorer.searchDirectory (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\Explorer.js:55:27)\n    at async run (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\Explorer.js:35:22)\n    at async cacheWrapper (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\cacheWrapper.js:16:18)\n    at async cacheWrapper (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\cacheWrapper.js:16:18)\n    at async cacheWrapper (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\cacheWrapper.js:16:18)\n    at async Explorer.search (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\cosmiconfig\\dist\\Explorer.js:27:20)\n    at async loadConfig (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\postcss-loader\\dist\\utils.js:68:16)\n    at async Object.loader (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\postcss-loader\\dist\\index.js:54:22)\n    at processResult (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\webpack\\lib\\NormalModule.js:890:19)\n    at D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\webpack\\lib\\NormalModule.js:1036:5\n    at D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\loader-runner\\lib\\LoaderRunner.js:400:11\n    at D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\loader-runner\\lib\\LoaderRunner.js:252:18\n    at context.callback (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at Object.loader (D:\\laragon\\www\\KnowledgeBaseSystem\\node_modules\\postcss-loader\\dist\\index.js:56:7)");
+
+/***/ }),
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module './bootstrap'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	__webpack_modules__["./resources/js/app.js"](0, {}, __webpack_require__);
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/css/app.css"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
